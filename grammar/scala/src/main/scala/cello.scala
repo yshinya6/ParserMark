40ce34aa9c56e284575aae72176df32bce65cb21
t-ryo <t.ryosuke.fv@gmail.com>
2016-11-02 18:29:19 +0900

    end scala development (3)

--- grammar/scala/src/main/scala/cello.scala
+++ grammar/scala/src/main/scala/cello.scala
@@ -5,62 +5,49 @@
 import scala.util.parsing.combinator._
 
 object CelloParser extends RegexParsers {
-  def Program = TopLevel | ( Program ~ TopLevel )
-  //def Program = TopLevel | ( HOGE ~ TopLevel )
+  def Program = TopLevel ~ ( TopLevel ).*
   def TopLevel = ( ImportDeclarations ~ Declaration ) | Declaration | ';'
-  def ImportDeclarations = ImportDeclaration | ( ImportDeclarations ~ ImportDeclaration )
-  //def ImportDeclarations = ImportDeclaration | ( HOGE ~ ImportDeclaration )
+  def ImportDeclarations = ImportDeclaration ~ ( ImportDeclaration ).*
   def ImportDeclaration = IMPORT ~ PackageName ~ ';'
   def PackageName = QualifiedName ~ '.' ~ '*'
   def Declaration = FunctionDeclaration | VariableDeclaration
   def FunctionDeclaration = ( Type ~ NAME ~ '(' ~ FunctionParamList ~ ')' ~ Block ) | ( Type ~ NAME ~ '(' ~ FunctionParamList ~ ')' ~ ';' ) | ( Type ~ NAME ~ '(' ~ ')' ~ Block ) | ( Type ~ NAME ~ '(' ~ ')' ~ ';' ) | ( Type ~ Block )
-  def FunctionParamList = FunctionParam | ( FunctionParamList ~ ',' ~ FunctionParam ) //| ',' ~ VAR_LEN_PARAM
-  //def FunctionParamList = FunctionParam | ( HOGE ~ ',' ~ FunctionParam ) //| ',' ~ VAR_LEN_PARAM
+  // def FunctionParamList = FunctionParam | ( FunctionParamList ~ ',' ~ FunctionParam ) | ',' ~ VAR_LEN_PARAM
+  def FunctionParamList = ( FunctionParam | ( ',' ~ VAR_LEN_PARAM ) ) ( ',' ~  FunctionParam | ( ',' ~ VAR_LEN_PARAM )).*
   def FunctionParam = Type | ( Type ~ NAME )
   def Block = ( '{' ~ BlockInner ~ '}' ) | '{' ~ '}'
-  def BlockInner = Statement | Declaration | ( BlockInner ~ Statement ) | ( BlockInner ~ Declaration )
-  //def BlockInner = Statement | Declaration | ( HOGE ~ Statement ) | ( HOGE ~ Declaration )
+  def BlockInner = ( Statement | Declaration ) ~ ( Statement | Declaration ).*
   def Statement = Block | IfStatement | ReturnStatement | ExpressionStatement | ';'
   def IfStatement = ( IF ~ '(' ~ Expression ~ ')' ~ Block ) | ( IF ~ '(' ~ Expression ~ ')' ~ Block ~ ELSE ~ Block )
   def ReturnStatement = ( RETURN ~ ';' ) | ( RETURN ~ Expression ~ ';' )
   def ExpressionStatement = Expression ~ ';'
   def VariableDeclaration = Type ~ VariableList ';'
-  def VariableList = InitDecl | ( VariableList ~ ',' ~ InitDecl )
-  //def VariableList = InitDecl | ( HOGE ~ ',' ~ InitDecl )
+  def VariableList = InitDecl ~ ( ',' ~ InitDecl ).*
   def InitDecl = NAME | ( NAME ~ '=' ~ Initializer )
   def Initializer = AssignmentExpression
   def Type = INT_TYPE | BOOLEAN_TYPE | STRING_TYPE | LONG_TYPE
-  def Expression = AssignmentExpression | ( Expression ~ ',' ~ AssignmentExpression )
-  //def Expression = AssignmentExpression | ( HOGE ~ ',' ~ AssignmentExpression )
-  def AssignmentExpression = ( UnaryExpression ~ '=' ~ AssignmentExpression ) | ConditionalExpression //FIXME
-  //def AssignmentExpression = ( UnaryExpression ~ '=' ~ HOGE ) | ConditionalExpression
-  def ConditionalExpression = LogicalANDExpression | ( ConditionalExpression ~ OR ~ LogicalANDExpression )
-  //def ConditionalExpression = LogicalANDExpression | ( HOGE ~ OR ~ LogicalANDExpression )
-  def LogicalANDExpression = EqualityExpression | ( LogicalANDExpression ~ AND ~ EqualityExpression )
-  //def LogicalANDExpression = EqualityExpression | ( HOGE ~ AND ~ EqualityExpression )
-  def EqualityExpression = RelationalExpression | ( EqualityExpression ~ EQ ~ RelationalExpression ) | ( EqualityExpression ~ NE ~ RelationalExpression )
-  //def EqualityExpression = RelationalExpression | ( HOGE ~ EQ ~ RelationalExpression ) | ( HOGE ~ NE ~ RelationalExpression )
-  def RelationalExpression = UnaryExpression | ( RelationalExpression ~ '<' ~ UnaryExpression ) | ( RelationalExpression ~ '>' ~ UnaryExpression )
-  //def RelationalExpression = UnaryExpression | ( HOGE ~ '<' ~ UnaryExpression ) | ( HOGE ~ '>' ~ UnaryExpression )
-  def UnaryExpression = PostfixExpression | ( '!' ~ UnaryExpression )
-  //def UnaryExpression = PostfixExpression | ( '!' ~ HOGE )
+  def Expression = AssignmentExpression ~ ( ',' ~ AssignmentExpression ).*
+  def AssignmentExpression = ( UnaryExpression ~ '=' ).* ~ ConditionalExpression //FIXME
+  def ConditionalExpression = LogicalANDExpression ~ ( OR ~ LogicalANDExpression ).*
+  def LogicalANDExpression = EqualityExpression ~ ( AND ~ EqualityExpression ).*
+  def EqualityExpression = RelationalExpression ~ ( ( EQ | NE ) ~ RelationalExpression ).*
+  def RelationalExpression = UnaryExpression ~ (( '<' | '>' ) ~ UnaryExpression ).*
+  def UnaryExpression = '!'.* ~ PostfixExpression
   def PostfixExpression = PrimaryExpression | ( PrimaryExpression ~ FunctionCall )
   def FunctionCall = ( '(' ~ ArgumentExpressionList ~ ')' ) | ( '(' ~ ')' )
-  def ArgumentExpressionList = AssignmentExpression | ( ArgumentExpressionList ~ ',' ~ AssignmentExpression )
-  //def ArgumentExpressionList = AssignmentExpression | ( HOGE ~ ',' ~ AssignmentExpression )
+  def ArgumentExpressionList = AssignmentExpression ~ ( ',' ~ AssignmentExpression ).*
   def PrimaryExpression = Literal | ( '(' ~ Expression ~ ')' )
-  def QualifiedName = NAME | ( QualifiedName ~ '.' ~ NAME )
-  //def QualifiedName = NAME | ( HOGE ~ '.' ~ NAME )
+  def QualifiedName = NAME ~ ( '.' ~ NAME ).*
   def Literal = INT | TRUE | FALSE | STRING | NULL_LITERAL | NAME
 
-  //def VAR_LEN_PARAM =
-  def EQ = '='
+  def VAR_LEN_PARAM = "..."
+  def EQ = "=="
   def NE = "!="
   def AND = "&&"
   def OR = "||"
-  def STRING_TYPE = "String"
+  def STRING_TYPE = "string"
   def INT_TYPE = "int"
-  def BOOLEAN_TYPE = "bool"
+  def BOOLEAN_TYPE = "boolean"
   def LONG_TYPE = "long"
   def IF = "if"
   def FOR = "for"
@@ -71,12 +58,8 @@ object CelloParser extends RegexParsers {
   def IMPORT = "import"
   def INT = """[0-9][1-9].*""".r
   def STRING = "[a-zA-Z]+".r
-  def NAME = STRING
+  def NAME = "[a-zA-Z0-9_]+".r
   def NULL_LITERAL = "null"
-
-
-  def HOGE = ' '
-
 
 
 
@@ -85,6 +68,4 @@ object CelloParser extends RegexParsers {
     case NoSuccess(errorMessage, next) => Left(s"$errorMessage on line ${next.pos.line} on column ${next.pos.column}")
   }
 }
-
-println(CelloParser("a"))
 
