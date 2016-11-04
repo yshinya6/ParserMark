6e3455ded828c540625a40248c380afdfb7aaac2
t-ryo <t.ryosuke.fv@gmail.com>
2016-11-04 17:36:00 +0900

    end scala development(6)

--- grammar/scala/src/main/scala/cello.scala
+++ grammar/scala/src/main/scala/cello.scala
@@ -3,42 +3,43 @@
   */
 
 import scala.util.parsing.combinator._
+import scala.io.Source
+import scala.collection.mutable.StringBuilder
 
 object CelloParser extends RegexParsers {
   def Program = TopLevel ~ ( TopLevel ).*
-  def TopLevel = ( ImportDeclarations ~ Declaration ) | Declaration | ';'
+  def TopLevel = ( ImportDeclarations ~ Declaration ) | Declaration | ";"
   def ImportDeclarations = ImportDeclaration ~ ( ImportDeclaration ).*
-  def ImportDeclaration = IMPORT ~ PackageName ~ ';'
-  def PackageName = QualifiedName ~ '.' ~ '*'
+  def ImportDeclaration = IMPORT ~ PackageName ~ ";"
+  def PackageName = QualifiedName ~ ".*?".r
   def Declaration = FunctionDeclaration | VariableDeclaration
-  def FunctionDeclaration = ( Type ~ NAME ~ '(' ~ FunctionParamList ~ ')' ~ Block ) | ( Type ~ NAME ~ '(' ~ FunctionParamList ~ ')' ~ ';' ) | ( Type ~ NAME ~ '(' ~ ')' ~ Block ) | ( Type ~ NAME ~ '(' ~ ')' ~ ';' ) | ( Type ~ Block )
-  // def FunctionParamList = FunctionParam | ( FunctionParamList ~ ',' ~ FunctionParam ) | ',' ~ VAR_LEN_PARAM
-  def FunctionParamList = ( FunctionParam | ( ',' ~ VAR_LEN_PARAM ) ) ( ',' ~  FunctionParam | ( ',' ~ VAR_LEN_PARAM )).*
-  def FunctionParam = Type | ( Type ~ NAME )
-  def Block = ( '{' ~ BlockInner ~ '}' ) | '{' ~ '}'
-  def BlockInner = ( Statement | Declaration ) ~ ( Statement | Declaration ).*
-  def Statement = Block | IfStatement | ReturnStatement | ExpressionStatement | ';'
-  def IfStatement = ( IF ~ '(' ~ Expression ~ ')' ~ Block ) | ( IF ~ '(' ~ Expression ~ ')' ~ Block ~ ELSE ~ Block )
-  def ReturnStatement = ( RETURN ~ ';' ) | ( RETURN ~ Expression ~ ';' )
-  def ExpressionStatement = Expression ~ ';'
-  def VariableDeclaration = Type ~ VariableList　';'
-  def VariableList = InitDecl ~ ( ',' ~ InitDecl ).*
-  def InitDecl = NAME | ( NAME ~ '=' ~ Initializer )
+  def FunctionDeclaration = ( Type ~ Block ) | ( Type ~ NAME ~ "();" ) | ( Type ~ NAME ~ "()" ~ Block ) | ( Type ~ NAME ~ "(" ~ FunctionParamList ~ ");" ) | ( Type ~ NAME ~ "(" ~ FunctionParamList ~ ")" ~ Block )
+  def FunctionParamList: Parser[Any] = FunctionParam | ( FunctionParamList ~ "," ~ FunctionParam ) | "," ~ VAR_LEN_PARAM
+  def FunctionParam = ( Type ~ NAME ) | Type
+  def Block = "{}" | ( "{" ~ BlockInner ~ "}" )
+  def BlockInner: Parser[Any] = ( Statement | Declaration ) ~ ( Statement | Declaration ).*
+  def Statement: Parser[Any] = Block | IfStatement | ReturnStatement | ExpressionStatement | ";"
+  def IfStatement = ( IF ~ "(" ~ Expression ~ ")" ~ Block ) | ( IF ~ "(" ~ Expression ~ ")" ~ Block ~ ELSE ~ Block )
+  def ReturnStatement = ( RETURN ~ ";" ) | ( RETURN ~ Expression ~ ";" )
+  def ExpressionStatement = Expression ~ ";"
+  def VariableDeclaration = Type ~ VariableList ~ ";"
+  def VariableList = InitDecl ~ ( "," ~ InitDecl ).*
+  def InitDecl = ( NAME ~ "=" ~ Initializer ) | NAME
   def Initializer = AssignmentExpression
   def Type = INT_TYPE | BOOLEAN_TYPE | STRING_TYPE | LONG_TYPE
-  def Expression = AssignmentExpression ~ ( ',' ~ AssignmentExpression ).*
-  def AssignmentExpression = ( UnaryExpression ~ '=' ).* ~ ConditionalExpression //FIXME
+  def Expression = AssignmentExpression ~ ( "," ~ AssignmentExpression ).*
+  def AssignmentExpression = ( UnaryExpression ~ "!==".r ~ "=" ).* ~ ConditionalExpression //FIXME
   def ConditionalExpression = LogicalANDExpression ~ ( OR ~ LogicalANDExpression ).*
   def LogicalANDExpression = EqualityExpression ~ ( AND ~ EqualityExpression ).*
   def EqualityExpression = RelationalExpression ~ ( ( EQ | NE ) ~ RelationalExpression ).*
-  def RelationalExpression = UnaryExpression ~ (( '<' | '>' ) ~ UnaryExpression ).*
-  def UnaryExpression = '!'.* ~ PostfixExpression
+  def RelationalExpression = UnaryExpression ~ ( "<|>".r  ~ UnaryExpression ).*
+  def UnaryExpression = "!*".r ~ PostfixExpression
   def PostfixExpression = ( PrimaryExpression ~ FunctionCall ) | PrimaryExpression
-  def FunctionCall = ( '(' ~ ArgumentExpressionList ~ ')' ) | ( '(' ~ ')' )
-  def ArgumentExpressionList = AssignmentExpression ~ ( ',' ~ AssignmentExpression ).*
-  def PrimaryExpression = Literal | ( '(' ~ Expression ~ ')' )
-  def QualifiedName = NAME ~ ( '.' ~ NAME ).*
-  def Literal = INT | TRUE | FALSE | STRING | NULL_LITERAL | NAME
+  def FunctionCall: Parser[Any] = ( "()" ) | ( "(" ~ ArgumentExpressionList ~ ")" )
+  def ArgumentExpressionList = AssignmentExpression ~ ( "," ~ AssignmentExpression ).*
+  def PrimaryExpression : Parser[Any] = ( "(" ~ Expression ~ ")" ) | Literal
+  def QualifiedName = NAME ~ ( "." ~ NAME ).*
+  def Literal = INT | TRUE | FALSE | NULL_LITERAL | STRING | NAME
 
   def VAR_LEN_PARAM = "..."
   def EQ = "=="
@@ -56,16 +57,33 @@ object CelloParser extends RegexParsers {
   def FALSE = "false"
   def TRUE = "true"
   def IMPORT = "import"
-  def INT = """[0-9][1-9].*""".r
+  def INT = "[0-9][1-9]*".r
   def STRING = "[a-zA-Z]+".r
   def NAME = "[a-zA-Z0-9_]+".r
   def NULL_LITERAL = "null"
 
 
 
-  def apply(input: String): Either[String, Any] = parseAll(file, input) match {
-    case Success(csvData, next) => Right(csvData)
+  def apply(input: String): Either[String, Any] = parseAll(Program, input) match {
+    case Success(postalCodeData, next) => Right(postalCodeData)
     case NoSuccess(errorMessage, next) => Left(s"$errorMessage on line ${next.pos.line} on column ${next.pos.column}")
   }
 }
 
+object input{
+  def main (args:Array[String]):Unit={
+    val source2 = Source.fromFile("text.txt", "UTF-8")
+    val aBuffer = new StringBuilder
+    try {
+      for (line <- source2.getLines) {
+        aBuffer.append(line)
+      }
+    }
+    finally {
+      source2.close
+    }
+    println(CelloParser(aBuffer.toString))
+
+    //println(CelloParser("int f(){\n  int a = 1;\n}"))
+  }
+}
