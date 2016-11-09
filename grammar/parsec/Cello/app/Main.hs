dcb76bf8a32416dfcc8ce6312bfc44a5b02d02fd
Daisuke <Yamagutino@greenApple.local>
2016-11-09 18:54:44 +0900

    added timer in Parsec

--- grammar/parsec/Cello/app/Main.hs
+++ grammar/parsec/Cello/app/Main.hs
@@ -4,7 +4,16 @@ import CelloParser
 import Control.Monad
 import Text.Parsec
 import Data.Char
+import System.Time
 
 main = do
   l <- getContents
+  start <- getClockTime
   parseTest programParser l
+  getClockTime >>= (\time -> return (diffClockTimes time start)) >>= (\ps -> putStr (muToM (show (tdPicosec ps `div` 1000000))))
+
+muToM :: String -> String
+muToM s = insert $ splitAt 2 s
+  where
+    insert :: (String, String)-> String
+    insert xs = fst xs ++ "." ++ snd xs ++ "[ms]"
