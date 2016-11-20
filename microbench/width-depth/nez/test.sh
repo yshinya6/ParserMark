0e61d3c124abf5608cadb30574a269ea80593c0e
yshinya6 <y.shinya.kml@gmail.com>
2016-11-20 15:49:55 +0900

    [microbench] added test.sh for width-depth test

--- microbench/width-depth/nez/test.sh
+++ microbench/width-depth/nez/test.sh
@@ -1,4 +1,6 @@
-for x in {0..61}; do
-  nez mybench -Draw=true -g 100-${x}.nez ../test/100-${x}.txt
-  # nez mybench -g ${x}.nez ../test/${x}.txt
+for width in 20 30 40 50; do
+  for x in {0..61}; do
+    nez mybench -Draw=true -g ${width}-${x}.nez ../test/${width}-${x}.txt
+    # nez mybench -g ${x}.nez ../test/${x}.txt
+  done
 done
