c77f7599af14eb1cc634bc53ed34667405a778c9
yshinya6 <y.shinya.kml@gmail.com>
2017-01-19 19:44:13 +0900

    [Lua] modified for benchmark

--- grammar/LuaPEG/bench.sh
+++ grammar/LuaPEG/bench.sh
@@ -1,29 +1,8 @@
-#!/bin/sh
-
-XML_LUA="xml-classic.lua"
-JS_LUA="javascript4.lua"
-JAVA_LUA="java8.lua"
-XML="../../source/xml"
-JAVA="../../source/java"
-JS="../../source/js"
-
-echo "\n#### bench xml files ####"
-echo "input, latency[ms]"
-for file in ${XML}/*.xml
-do
-  lua ${XML_LUA} ${file}
+for x in {1..9};do
+  lua epsilon.lua ../../source/epsilon/${x}k.epsilon
 done
 
-# echo "\n#### bench js files ####"
-# echo "input, latency[ms]"
-# for file in ${JS}/*.js
-# do
-#   lua ${JS_LUA} ${file}
-# done
-
-echo "\n#### bench java files ####"
-echo "input, latency[ms]"
-for file in ${JAVA}/*.java
-do
-  lua ${JAVA_LUA} ${file}
+for x in {1..10};do
+  lua epsilon.lua ../../source/epsilon/${x}0k.epsilon
 done
+
