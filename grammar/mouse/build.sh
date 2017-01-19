a95faa367738c7b703705d908737ec4afb174409
yshinya6 <y.shinya.kml@gmail.com>
2017-01-19 16:41:17 +0900

    [Mouse] modfied for benchmark

--- grammar/mouse/build.sh
+++ grammar/mouse/build.sh
@@ -1,4 +1,5 @@
 java -cp Mouse-1.6.1.jar: mouse.Generate -G resource/Epsilon.peg -P Epsilon 
-gsed -i -e "1i package resource;" Epsilon.java
+#gsed -i -e "1i package resource;" Epsilon.java
+sed -i -e "1i package resource;" Epsilon.java
 mv Epsilon.java resource/
 javac -cp Mouse-1.6.1.jar: resource/Epsilon.java
